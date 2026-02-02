1096d7ae74796ab5173d1b64611ddab8f35ea758
JinHxxxxKim <rlawlsgud217@gmail.com>
2026-02-02 18:09:41 +0900

    chore: project be upload and setting

---
+++ jasan-be/infra-eureka/src/test/java/com/jasan/infraeureka/InfraEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasan.infraeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ jasan-be/service-auth/src/test/java/com/jasan/serviceauth/ServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasan.serviceauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
